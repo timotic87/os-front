81ad012bc0e0a37c8413dcb989f767cf5d2b2b45
Bojan Timotic <timotic87@gmail.com>
2025-01-22 21:33:54 +0100

    deasls filters

--- src/app/deals/deals.component.ts
+++ src/app/deals/deals.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import {DatePipe} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {DatePipe, NgClass, NgIf} from "@angular/common";
 import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { RestService } from '../services/rest.service';
 
 @Component({
@@ -9,20 +9,42 @@ import { RestService } from '../services/rest.service';
   standalone: true,
   imports: [
     FormsModule,
-    DatePipe
+    DatePipe,
+    ReactiveFormsModule,
+    NgClass,
+    NgIf
   ],
   templateUrl: './deals.component.html',
   styleUrl: './deals.component.css'
 })
-export class DealsComponent {
+export class DealsComponent implements OnInit {
 
   searchText
 
   deals;
 
+  filterForm: FormGroup;
+
+  filteredData;
+
   constructor(private rest: RestService) {
     this.updateDealsList()
   }
+
+  ngOnInit(): void {
+    this.filterForm = new FormGroup({
+      isExpired: new FormControl(''),
+      isAssigned: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl(''),
+      clientName: new FormControl(''),
+      bdConsultant: new FormControl(''),
+      hrAdmin: new FormControl(''),
+      status: new FormControl({value:'', disabled: true}, )
+    });
+
+    this.filteredData = [...this.deals];
+    }
 
   search(){
 
@@ -36,9 +58,32 @@ export class DealsComponent {
     this.rest.getDeals().subscribe(res=>{
       if (res.status === 200) {
         this.deals=res.data;
-        console.log(this.deals);
+        this.applyFilters()
       }
     });
   }
+
+  applyFilters(): void {
+    const filters = this.filterForm.value;
+    this.filteredData = this.deals.filter(item => {
+      const bdFullName = `${item.bdFirstName} ${item.bdLastName}`.toLowerCase();
+      const hraFullName = `${item.hraFirstName} ${item.hraLastName}`.toLowerCase();
+      const isSameDate = (date1: Date, date2: Date): boolean => {
+        return (
+          date1.getFullYear() === date2.getFullYear() &&
+          date1.getMonth() === date2.getMonth() &&
+          date1.getDate() === date2.getDate()
+        );
+      };
+      return (
+        (filters.isExpired === '' || item.isExpired === JSON.parse(filters.isExpired)) &&
+        (filters.isAssigned === '' || JSON.parse(filters.isAssigned) === (!!item.hraID)) &&
+        (!filters.startDate || isSameDate(new Date(item.startDate), new Date(filters.startDate))) &&
+        (!filters.endDate || isSameDate(new Date(item.endDate), new Date(filters.endDate))) &&
+        (!filters.clientName || item.clientName.toLowerCase().includes(filters.clientName.toLowerCase())) &&
+        (!filters.bdConsultant || bdFullName.includes(filters.bdConsultant.toLowerCase())) &&
+        (!filters.hrAdmin || hraFullName.includes(filters.hrAdmin.toLowerCase()))
+      );
+    });  }
 
 }
